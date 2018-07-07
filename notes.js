c38f0dfafbf6221608fe5319a8d3d4625c98b5f9
ssdorwat <ssdorwat@ncsu.edu>
2018-07-07 00:16:05 -0400

    initial commit

---
+++ notes.js
@@ -0,0 +1,12 @@
+console.log('starting notes.js')
+
+module.exports.addNotes = () => {
+    console.log('adding notes ');
+    return 'new note';
+}
+
+
+module.exports.add = (a, b) => {
+    console.log('adding notes ');
+    return a+b;
+}
